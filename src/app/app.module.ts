47dfb8a002f155b847356fbe47890c09b0af1c5e
paulogustavo <paulogustavo@outlook.com>
2020-09-24 01:41:29 -0300

    Shipping added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,20 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { ProductMoreDetailsComponent } from './product-more-details/product-more-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'products-more-details/:productId', component: ProductMoreDetailsComponent },
       { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent}
     ])
   ],
   declarations: [
@@ -30,7 +34,8 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     ProductMoreDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
